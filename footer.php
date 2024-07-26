a6b763dcb34907f9a743365f75fd0b873aa8700a
YhelTzy <bawegauriel4455@gmail.com>
2024-07-27 01:52:48 +0800

    Uriel Update

--- footer.php
+++ footer.php
@@ -1,4 +1,63 @@
     <footer>
+        <div class="container max-w-full max-h-[350px] bg-[#838383]">
+            <div id="footer_left" class="max-h-[350px] pl-[105px]">
+                <div class="flex justify-between">
+                    <div class="max-w-[1440px] w-full">
+                        <div class="grid md:grid-cols-5 gap-[50px]">
+                            <div class="max-w-[320px] max-h-[300]">
+                                <div class="flex flex-col">
+                                    <h1 class="font-roboto font-semibold text-[20px] text-white pt-[46px] pb-[30px]">Pages</h1>
+                                    <a href="#" class="text-white font-sans text-[15px] pb-[10px]">Home</a>
+                                    <a href="#" class="text-white font-sans text-[15px] pb-[10px]">About us</a>
+                                    <a href="#" class="text-white font-sans text-[15px] pb-[10px]">How it works</a>
+                                    <a href="#" class="text-white font-sans text-[15px] pb-[10px]">Blog</a>
+                                    <a href="#" class="text-white font-sans text-[15px]">Contact us</a>
+                                </div>
+                            </div>
+                            <div class="max-w-[320px] max-h-[300]">
+                                <div class="flex flex-col ">
+                                    <h1 class="font-roboto font-semibold text-[20px] text-white pt-[46px] pb-[30px]">Locations</h1>
+                                    <a href="#" class="text-white font-sans text-[15px] pb-[10px]">Bloomington Normal</a>
+                                    <a href="#" class="text-white font-sans text-[15px] pb-[10px]">Champaign Urbana</a>
+                                    <a href="#" class="text-white font-sans text-[15px] pb-[10px]">Decatur</a>
+                                    <a href="#" class="text-white font-sans text-[15px] pb-[10px]">Peoria</a>
+                                    <a href="#" class="text-white font-sans text-[15px] pb-[10px]">Springfield</a>
+                                    <a href="#" class="text-white font-sans text-[15px] pb-[10px]">Rockford</a>
+                                    <a href="#" class="text-white font-sans text-[15px]">Danville</a>
+                                </div>
+                            </div>
+                            <div class="max-w-[320px] max-h-[250]">
+                                <div class="flex flex-col ">
+                                    <h1 class="font-roboto font-semibold text-[20px] text-white pt-[46px] pb-[30px]">Contact</h1>
+                                    <a href="#" class="text-white font-sans text-[15px] pb-[10px]">T: (309) 704 - 0164</a>
+                                    <a href="#" class="text-white font-sans text-[15px] pb-[10px]">T: 217-600-2177</a>
+                                    <a href="#" class="text-white font-sans text-[15px] pb-[10px]">E: contact@centrilinvestors.com</a>
+                                </div>
+                            </div>
+                            <div class="max-w-[320px] max-h-[250]">
+                                <div class="flex flex-col ">
+                                    <h1 class="font-roboto font-semibold text-[20px] text-white pt-[46px] pb-[30px]">Social</h1>
+                                    <div class="flex">
+                                        <a href="#" class="text-white font-sans text-[15px] pb-[10px]">
+                                            <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 512 512" class=""><path fill="#ffffff" d="M512 256C512 114.6 397.4 0 256 0S0 114.6 0 256C0 376 82.7 476.8 194.2 504.5V334.2H141.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H287V510.1C413.8 494.8 512 386.9 512 256h0z"/></svg>
+                                        </a>
+                                        <a href="#" class="text-white font-sans text-[15px] pb-[10px]">
+                                            <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 448 512" class="ml-[15px]"><path fill="#ffffff" d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/></svg>
+                                        </a>
+                                        <a href="#" class="text-white font-sans text-[15px] pb-[10px]">
+                                            <svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 576 512" class="ml-[15px]"><path fill="#ffffff" d="M549.7 124.1c-6.3-23.7-24.8-42.3-48.3-48.6C458.8 64 288 64 288 64S117.2 64 74.6 75.5c-23.5 6.3-42 24.9-48.3 48.6-11.4 42.9-11.4 132.3-11.4 132.3s0 89.4 11.4 132.3c6.3 23.7 24.8 41.5 48.3 47.8C117.2 448 288 448 288 448s170.8 0 213.4-11.5c23.5-6.3 42-24.2 48.3-47.8 11.4-42.9 11.4-132.3 11.4-132.3s0-89.4-11.4-132.3zm-317.5 213.5V175.2l142.7 81.2-142.7 81.2z"/></svg>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="footer_right" class="max-w-[370px] w-full h-[350px] bg-[#3A3A3A] flex items-center justify-center">
+                        <img class="w-[150px]" src="<?= get_template_directory_uri() ?>/assets/images/logo.png" alt="logo">
+                    </div>
+                </div>
+            </div>
+        </div>
 
     </footer>
     <?php wp_footer(); ?>
--- templates/contact.php
+++ templates/contact.php
@@ -4,6 +4,108 @@
  */
 get_header();
 ?>
+    <section>
+        <div class="container max-w-[1920px] h-[800px] w-full">
+            <div class="w-full h-[800px]">
+                <div class="relative bg-cover bg-center w-full h-[800px] overlay overlay-dark" style="background-image: url(<?php echo get_template_directory_uri() . ('/assets/images/contact-banner.jpg')?>)">
 
+                     <div class="h-[800px]">
+                        <h1 class="text-white text-[60px] font-roboto font-semibold pt-[100px] text-center">Contact Us</h1>
+                            <p class="text-white text-[30px] font-sans pt-[50px] max-w-[1109px] text-center mx-auto">
+                                We'd love to hear from you! Whether you have a question about our services, want a cash offer on your property, or just want to say hi, feel free to reach out to us.
+                            </p>
+
+                        <div class="flex flex-col text-center pt-[25px]">
+                            <div class="container max-w-[550px] mx-auto">
+                                <div class="flex text-center justify-center pb-[30px]">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="30" width="30" viewBox="0 0 512 512" class=""><path fill="#FFD43B" d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"/></svg>
+                                    <p class="font-roboto text-white text-[25px] pl-[20px]">(309) 704-0164</p>
+                                </div>
+                                <div class="flex text-center justify-center pb-[30px]">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="30" width="30" viewBox="0 0 512 512" class=""><path fill="#FFD43B" d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"/></svg>
+                                    <p class="font-roboto text-white text-[25px] pl-[20px]">217-600-2177</p>
+                                </div>
+                                <div class="flex text-center justify-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="30" width="30" viewBox="0 0 512 512" class=""><path fill="#FFD43B" d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48L48 64zM0 176L0 384c0 35.3 28.7 64 64 64l384 0c35.3 0 64-28.7 64-64l0-208L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg>
+                                    <p class="font-roboto text-white text-[25px] pl-[20px]">contact@centrilinvestors.com</p>
+                                </div>
+                            </div>
+                        </div>
+                     </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="container max-w-full">
+            <div class="bg-[#EAEAEA] h-[1000px]">
+                <h1 class="font-roboto font-semibold text-black text-[30px] pt-[54px] text-center">Additional Information</h1>
+                <p class="font-sans text-black text-[20px] pt-[30px] pb-[40px] text-center max-w-[997px] mx-auto">If you have any specific questions about selling your house, you can reach out to us directly. We specialize in quick sales for cash and can help you through the entire process with no fees or commissions.</p>
+                <div class="container max-w-[1500px] h-[700px]">
+
+                    <div class="grid grid-cols-2 gap-[100px]">
+                        <div class="bg-white max-w-[700px] h-[700px] rounded-lg ">
+                            <div class="pt-[54px]">
+                                <form class="px-[70px]">
+                                    <div class="mb-5 max-w-[560px]">
+                                        <label for="email" class="block mb-[4.70px] text-[12px] font-roboto text-black ">Full Name</label>
+                                        <input type="email" id="email" class="bg-[#D9D9D9] rounded-[12px] border border-[#D9D9D9] block w-full p-2.5 font-roboto text-black text-[15px] pl-[31px] pr-[31px]" placeholder="Enter Full Name" required />
+                                    </div>
+                                    <div class="mb-5 max-w-[560px]">
+                                        <label for="email" class="block mb-[4.70px] text-[12px] font-roboto text-black ">Address</label>
+                                        <input type="email" id="email" class="bg-[#D9D9D9] rounded-[12px] border border-[#D9D9D9] block w-full p-2.5 font-roboto text-black text-[15px] pl-[31px] pr-[31px]" placeholder="Enter address" required />
+                                    </div>
+                                    <div class="mb-5 max-w-[560px]">
+                                        <label for="email" class="block mb-[4.70px] text-[12px] font-roboto text-black ">Phone</label>
+                                        <input type="email" id="email" class="bg-[#D9D9D9] rounded-[12px] border border-[#D9D9D9] block w-full p-2.5 font-roboto text-black text-[15px] pl-[31px] pr-[31px]" placeholder="Enter Phone Number" required />
+                                    </div>
+                                    <div class="mb-5 max-w-[560px]">
+                                        <label for="email" class="block mb-[4.70px] text-[12px] font-roboto text-black ">Email address</label>
+                                        <input type="email" id="email" class="bg-[#D9D9D9] rounded-[12px] border border-[#D9D9D9] block w-full p-2.5 font-roboto text-black text-[15px] pl-[31px] pr-[31px]" placeholder="Enter Email address" required />
+                                    </div>
+                                    <div class="mb-5 max-w-[560px]">
+                                        <label for="message" class="block mb-[4.70px] text-[12px] font-roboto text-black ">Message</label>
+                                        <textarea id="message" rows="5" class="block p-2.5 w-full font-roboto bg-[#D9D9D9] rounded-[12px] border border-[#D9D9D9] text-black text-[15px] pt-[13px] pl-[31px] pr-[31px]" placeholder="Write your message"></textarea>
+                                    </div>
+                                    <button class="rounded-full py-[16px] pl-[20px] pr-[20px] bg-[#3A3A3A] text-white">
+                                        <span class="flex justify-center items-center">
+                                            <a href="#" class="pr-[62px]">Submit</a>
+                                            <svg xmlns="http://www.w3.org/2000/svg" height="20" width="12.5" viewBox="0 0 320 512"><path fill="#ffffff" d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>
+                                        </span>
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+
+                        <div class="max-w-[700px] h-[700px] ">
+                            <h1 class="font-roboto font-semibold text-black text-[30px] pb-[50px]">Our Services Include</h1>
+                            <div class="flex flex-col">
+                                <div class="">
+                                    <div class="flex pl-[30px] pr-[20px] pb-[10px]">
+                                        <svg xmlns="http://www.w3.org/2000/svg" height="30" width="30" viewBox="0 0 512 512"><path fill="#FFD43B" d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-111 111-47-47c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64c9.4 9.4 24.6 9.4 33.9 0L369 209z"/></svg>
+                                        <p class="font-sans text-black text-[20px] pl-[20px] pb-[10px]">Buying properties in any condition</p>
+                                    </div>
+                                    <div class="flex pl-[30px] pr-[20px] pb-[10px]">
+                                        <svg xmlns="http://www.w3.org/2000/svg" height="30" width="30" viewBox="0 0 512 512"><path fill="#FFD43B" d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-111 111-47-47c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64c9.4 9.4 24.6 9.4 33.9 0L369 209z"/></svg>
+                                        <p class="font-sans text-black text-[20px] pl-[20px] pb-[10px]">Providing fair cash offers</p>
+                                    </div>
+                                    <div class="flex pl-[30px] pr-[20px] pb-[10px]">
+                                        <svg xmlns="http://www.w3.org/2000/svg" height="30" width="30" viewBox="0 0 512 512"><path fill="#FFD43B" d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-111 111-47-47c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64c9.4 9.4 24.6 9.4 33.9 0L369 209z"/></svg>
+                                        <p class="font-sans text-black text-[20px] pl-[20px] pb-[10px]">Covering all closing costs</p>
+                                    </div>
+                                    <div class="flex pl-[30px] pr-[20px]">
+                                        <svg xmlns="http://www.w3.org/2000/svg" height="30" width="30" viewBox="0 0 512 512"><path fill="#FFD43B" d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-111 111-47-47c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64c9.4 9.4 24.6 9.4 33.9 0L369 209z"/></svg>
+                                        <p class="font-sans text-black text-[20px] pl-[20px]">Flexible closing schedules</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
 
 <?php get_footer(); ?>
